2387165bf37ec00e9e5c15ee9a0e41a5134208c6
Muniraja <muniraja0799@gmail.com>
2022-11-15 10:44:01 +0530

    first commit

---
+++ adb.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+os.system("clear")
+
+print("Android ADB and FASTBOOT Installer")
+print("script by Munirajatech 0.1")
+print("")
+print("[1]Insatll ADB and FASTBOOT")
+print("[2]Uninstall ADB and FASTBOOT")
+print("")
+choice = input("Choice:")
+if choice == "1":
+	os.system("apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh")
+elif choice == "2":
+	os.system("apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/RemoveTools.sh -q && bash RemoveTools.sh")
